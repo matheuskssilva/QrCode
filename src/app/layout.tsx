963c42f92aa44541d59a5d41797cdf705ab8929c
Matheus Silva <matheuskauan2709@gmail.com>
2024-02-24 15:30:32 -0300

    feat: add icon shortcut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={roboto.className}>{children}</body>
+      <link rel="shortcut icon" href="qr-code-icon-vector.jpg"  />
     </html>
   );
 }
